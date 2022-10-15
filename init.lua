d214d1685f06e2f93b48ca20533a88133713f6da
Bituvo <90872694+Bituvo@users.noreply.github.com>
2022-10-15 11:06:20 -0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,30 @@
+local storage = minetest.get_mod_storage()
+
+minetest.register_chatcommand("set_status", {
+    description = "Set your public in-game status",
+
+    privs = {
+        shout = true;
+    },
+
+    func = function(name, status)
+        storage:set_string(name, status)
+        minetest.chat_send_all(name .. " is " .. status)
+
+        return true
+    end
+})
+
+minetest.register_chatcommand("get_status", {
+    description = "Get a players' in-game status",
+
+    privs = {
+        interact = true;
+    },
+
+    func = function(invoker, name)
+        minetest.chat_send_player(invoker, name .. " is " .. storage:get_string(name))
+
+        return true
+    end
+})
